6bd58c889e4201c1140bdb67ca72346dfcb60f30
protein_to_vitamin <67103938+proteinToVitamin@users.noreply.github.com>
2024-02-27 15:09:14 +0900

    Update CFD.cpp
    流速の予測値の計算の部分を追加。
    次は圧力修正子から書く

--- CFD.cpp
+++ CFD.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 // 計算空間のサイズと天井の速度 
 double const Lx = 1.;
@@ -41,15 +43,85 @@ int main(){
     double t = dt;
     while(t <= t_end){
         //step1：流速の予測値の計算
-        for(int i = 1; i <= Nx − 1; ++i){// upの計算
+        for(int i = 1; i <= Nx - 1; ++i){// upの計算
             for(int j = 0; j <= Ny - 1; ++j){
                 
-                double uw = (u[i−1][j]+u[i][j])/2.; 
+                double uw = (u[i-1][j]+u[i][j])/2.; 
                 double ue = (u[i][j]+u[i][j+1])/2.;
+
+                double uux = ((ue*ue)-(uw*uw))/dx;
+
+                double us, vs, un, vn;
+                if (0 == j) { 
+                    us = vs = 0.;
+                } 
+                else {
+                us = 0.5*(u[i][j-1] + u[i][j]); vs = 0.5*(v[i-1][j] + v[i][j]);
+                }
+                if (Ny-1 == j) {
+                un = U;
+                vn = 0.; } 
+                else {
+                un = 0.5*(u[i][j] + u[i][j+1]);
+                vn = 0.5*(v[i-1][j+1] + v[i][j+1]); 
+                }
+                double uvy = (un*vn-us*vs)/dy;
+
+                double dpdx = (p[i][j] - p[i-1][j])/dx;
+
+                double uxx = (u[i-1][j]-2*u[i][j] + u[i+1][j])/(dx*dx); 
+                double uyy ;
+                if (0 == j){
+                    double us = 0.;
+                    uyy = (8/3*us - (8/3+4/3)*u[i][j] + 4/3*u[i][j +1])/(dy*dy);
+                            }
+                up[i][j] = u[i][j] + dt*(-(uux+uvy) -dpdx + nu*(uxx+uyy));
             }
         }
+
         for(int i = 0; i <= Nx - 1; ++i){// vpの計算
-            for(int j = 1; j <= Ny - 1; ++j)
+            for(int j = 1; j <= Ny - 1; ++j){
+                //対流項x
+                double uw, vw, ue, ve;
+                if (0 == i) {
+                    uw = vw = 0;
+                }
+                else {
+                    uw = 0.5*(u[i][j-1] + u[i][j]); vw = 0.5*(v[i-1][j] + v[i][j]);
+                }
+                if (Nx - 1 == i) 
+                { ue = ve = 0;
+                } 
+                else {
+                    ue = 0.5*(u[i+1][j-1] + u[i+1][j]); ve = 0.5*(v[i][j] + v[i+1][j]);
+                    }
+                    double vux = (-(vw*uw)+(ve*ue))/dx;
+                //対流項y
+                double vs = 0.5*(v[i][j-1] + v[i][j]); 
+                double vn = 0.5*(v[i][j] + v[i][j+1]);
+                double vvy = (-vs*vs + vn*vn)/dy; 
+
+                // 圧力勾配項 y
+                double dpdy = (-p[i][j-1] + p[i][j])/dy;
+                //粘性項y 
+                double vxx ;
+                if (0 == i) {
+                double vw = 0.;
+                vxx = (8./3.*vw - (8./3.+4./3)*v[i][j] + 4./3.*v[i+1][j])/(dx*
+                dx);
+                } 
+                else if (Nx - 1 == i) {
+                double ve = 0;
+                vxx = (4./3.*v[i-1][j] - (8./3.+4./3)*v[i][j] + 8./3.*ve)/(dx*
+                dx); } 
+                else{
+                     vxx = (v[i-1][j] - 2*v[i][j] + v[i+1][j])/(dx*dx);
+                }
+
+                double vyy = (v[i][j-1] - 2*v[i][j] + v[i][j+1])/(dy*dy);
+
+                vp[i][j] = v[i][j] + dt*(-(vux+vvy) -dpdy + nu*(vxx+vyy));
+            }
         }
         //step2：圧力の修正量の計算
         //step3：圧力と流速の修正
